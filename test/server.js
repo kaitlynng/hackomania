5c802664f4123824d8faeba84858ef73ce138b0b
newaij <poontzeyang@gmail.com>
2019-02-23 14:10:48 +0800

    addded server functions for simple objects

--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
 //require the js packages used in the application
 var express = require("express");
+var mongoose = require('mongoose');
 var http = require("http");
 var path = require("path");
 var socketIO = require("socket.io");
@@ -7,7 +8,63 @@ var app = express();
 
 var server = http.Server(app);
 var io = socketIO(server); //note: set to listen on HTTP server
+//STUFF FOR MONGO
+mongoose.connect('mongodb://127.0.0.1/gridFS',{useNewUrlParser: true});
+var conn = mongoose.connection;
 
+//-----------------------------mongo database functions---------------------------------------------------
+/*
+THERE ARE 2 KINDS OF OBJECTS: SIMPLE OBJECTS (almost everything) and AUDIOFILE OBJECTS
+*/
+//GET ANY SIMPLE OBJECTS
+function getObject(collection_name,conditions,callback){
+    conn.collection(collection_name).find(conditions).toArray(function(err,docs){
+      if (callback && typeof callback === 'function') {
+        callback(docs, "GOT:");
+      };
+    })
+}
+//POST TRANSCRIPT OBJECT (TRANSCRIPT OBJECT IS A SIMPLE OBJECT)
+var transcriptSchema = new mongoose.Schema({audio_id:'string',transText:'string'});
+var transcriptModel = mongoose.model('transcript',transcriptSchema,'transcripts');
+
+function postTranscript(new_text,callback){
+  var transcriptText = new transcriptModel({audio_id:'id_placeholder',transText:new_text});
+  transcriptText.save(function(err){
+    if (err) return handleError(err);
+    transcriptModel.findOne({transText:new_text}).exec(function(err,trans){
+      if (callback && typeof callback === 'function') {
+        callback(trans, "POSTED:");
+      };
+    });
+  });
+}
+
+function deleteObject(collection_name,conditions,callback){
+  conn.collection(collection_name).find(conditions).toArray(function(err,results){
+    if (callback && typeof callback === 'function') {
+      callback(results, "DELETED:");
+    };
+    conn.collection(collection_name).deleteMany(conditions);
+  });
+};
+
+function callconsole(arg,string){
+  console.log(string);
+  console.log(arg);
+};
+
+conn.once('open',()=>{
+  /*
+  postTranscript("wabalubba",callconsole);
+  getObject('transcripts',{transText:"wabalubba"},callconsole);
+  deleteObject('transcripts',{transText:'wabalubba'},callconsole);
+  */
+});
+
+
+
+//-------------------------------------------------------------------------------------------------
 app.set("port", 8000);
 
 app.use(express.static(path.join(__dirname+"/public")));
@@ -57,3 +114,4 @@ io.on("connection", function (socket) { //new instance is created with each new 
     io.emit("playerDisconnect", socket.id);
   });
 });
+
