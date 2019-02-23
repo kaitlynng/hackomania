22e3c913eca5c7f04abdd5f7b0cabcd88aa03947
newaij <poontzeyang@gmail.com>
2019-02-23 23:02:21 +0800

    sorry I need to test

---
+++ testserver.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+app.get('/about', function (req, res) {
+  res.send('about')
+})
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3001,"0.0.0.0");
